bcab4507b2335c5bce64da69100ec7c10257ffa8
Sean <smrutled@gmail.com>
2022-04-12 19:43:19 -0700

    create index.html instead of app.html

--- build_web.ps1
+++ build_web.ps1
@@ -5,4 +5,4 @@ cd emsdk
 cd ..
 New-Item -Name "web" -ItemType "directory"
 cd CSE240Project
-emcc Collision.cpp CSurface.cpp Cursor.cpp Ball.cpp Game.cpp Input_Events.cpp Line.cpp main.cpp -o ..\web\app.html --preload-file assets -Wall -g -lm -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2
+emcc Collision.cpp CSurface.cpp Cursor.cpp Ball.cpp Game.cpp Input_Events.cpp Line.cpp main.cpp -o ..\web\index.html --preload-file assets -Wall -g -lm -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2
